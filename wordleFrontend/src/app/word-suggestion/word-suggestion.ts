9bcbe05ed59ae8dd831c1bde26c5fd1a2461c205
Laura Pllana <laura.pllana@icloud.com>
2025-12-13 23:13:59 +0100

    word suggestion

--- wordleFrontend/src/app/word-suggestion/word-suggestion.ts
+++ wordleFrontend/src/app/word-suggestion/word-suggestion.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-word-suggestion',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './word-suggestion.html',
-  styleUrl: './word-suggestion.css',
+  styleUrls: ['./word-suggestion.css']
 })
 export class WordSuggestion {
 
+  word: string = '';
+  role: string = 'student';
+  definition: string = '';
+
+  suggestions: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  loadSuggestions() {
+    this.http.get<any[]>('http://localhost:8080/words')
+      .subscribe(data => {
+        this.suggestions = data.filter(w =>
+          w.word.startsWith(this.word.toLowerCase())
+        );
+      });
+  }
+
+  saveWord() {
+    const newWord = {
+      word: this.word.toLowerCase(),
+      role: this.role,
+      definition: this.definition
+    };
+
+    this.http.post('http://localhost:8080/words', newWord)
+      .subscribe(() => {
+        this.word = '';
+        this.definition = '';
+        this.loadSuggestions();
+      });
+  }
 }
