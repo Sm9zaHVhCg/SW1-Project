f52acb6aee933fd2e33d4f13e74eb0d955ca6c73
Laura Pllana <laura.pllana@icloud.com>
2025-12-15 22:16:37 +0100

    update word suggestion page (rebasing to resolve merge conflicts)

--- wordleBackend/src/main/java/de/dhbwstuttgart/wordlebackend/model/Word.java
+++ wordleBackend/src/main/java/de/dhbwstuttgart/wordlebackend/model/Word.java
@@ -1,31 +1,20 @@
 package de.dhbwstuttgart.wordlebackend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Entity
-@Table(name = "words")
 public class Word {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String word;
+    private String role;
 
-    private String role; 
-
-    private String Studiengang;
-
+    @Column(length = 500)
     private String definition;
-
-    public Word() {
-    }
-
-    public Word(String word, String role, String Studiengang ,String definition) {
-        this.word = word;
-        this.role = role;
-        this.definition = definition;
-    }
 
     public Long getId() {
         return id;
@@ -45,13 +34,6 @@ public class Word {
 
     public void setRole(String role) {
         this.role = role;
-    }
-
-    public String getStudiengang() {
-        return Studiengang;
-    }
-    public void setStudiengang(String Studiengang) {
-        this.Studiengang = Studiengang;
     }
 
     public String getDefinition() {
