b0747b7aa86fdc559656c12662073a9202cff69d
QwQ-24 <qinwen.wu@gmx.de>
2025-12-23 23:05:35 +0100

    Fix small bug noticed during testing.

--- wordleFrontend/src/app/admin/admin.spec.ts
+++ wordleFrontend/src/app/admin/admin.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Admin } from './admin';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
