bf0735bf88bfe347eef7fae66cfe752dcf1d6624
Joshua <inf24176@lehre.dhbw-stuttgart.de>
2025-12-23 20:34:12 +0100

    Docker compose + frontend dockerfile

--- wordleBackend/src/main/java/de/dhbwstuttgart/wordlebackend/repository/WordRepository.java
+++
@@ -1,8 +0,0 @@
-package de.dhbwstuttgart.wordlebackend.repository;
-
-import de.dhbwstuttgart.wordlebackend.model.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WordRepository extends JpaRepository<Word, Long> {
-}
-
